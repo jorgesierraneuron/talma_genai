85c66a3ba8498ccc34a2c7f15892b3be90e65a19
Jorge Sierra <jorge.sierra@rappi.com>
2025-01-20 11:30:09 -0500

    add uvicorn run

--- lambda_source/rethrieve_qa/app/main.py
+++ lambda_source/rethrieve_qa/app/main.py
@@ -1,6 +1,7 @@
 import json
 from fastapi import FastAPI, HTTPException
 from mangum import Mangum
+import uvicorn
 from models import SimilarityRequest
 from utils import search_filtered, search_unfiltered, generate_cause_analysis
 from fastapi.middleware.cors import CORSMiddleware
@@ -55,3 +56,5 @@ def similarity_search_unfiltered(request: SimilarityRequest):
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error en la búsqueda sin filtro: {str(e)}")
 
+if __name__ == "__main__":
+    uvicorn.run(app, port=8000)

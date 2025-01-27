19f23ffc5d52bb4e6ad3d6296582602c5850cfc4
jorsie <jorsie@amazon.com>
2025-01-26 23:09:33 -0500

    devolver cambios

--- lambda_source/rethrieve_qa/app/test.bash
+++
@@ -1,5 +0,0 @@
-mkdir -p models/processor
-python -c "
-from colpali_engine.models import ColPali, ColPaliProcessor;
-ColPali.from_pretrained('vidore/colpali-v1.3').save_pretrained('models/model');
-ColPaliProcessor.from_pretrained('vidore/colpaligemma-3b-pt-448-base').save_pretrained('models/processor')"
